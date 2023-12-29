b4f3bfc88a0861a2152159d97041a42858345422
pilgyu <pilgyu2003@gmail.com>
2023-12-29 16:19:52 +0900

    _

--- C_winter/29.12.23/main.c
+++ C_winter/29.12.23/main.c
@@ -8,6 +8,15 @@ int main(void) {
     printf("%10.3f\n",1.23456789);
     printf("%-10.3f\n",1.23456789);
     printf("%.3f\n",1.23456789);
+    
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of long long: %zu bytes\n", sizeof(long long));
+    printf("Size of pointer: %zu bytes\n", sizeof(void*));
+    
     return 0;
 
 }
