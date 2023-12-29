1f13ed4b6739af3e176a7b0d80b1556c20ee3ab8
pilgyu <pilgyu2003@gmail.com>
2023-12-29 14:33:14 +0900

    필드폭과 정밀도

---
+++ C_winter/29.12.23/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("0123456789\n");
+    // printf("%10d\n",123);
+    // printf("%-10d\n",123);
+    printf("%10.3f\n",1.23456789);
+    printf("%-10.3f\n",1.23456789);
+    printf("%.3f\n",1.23456789);
+    return 0;
+
+}
