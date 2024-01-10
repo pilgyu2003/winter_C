325e5e58b8c687306a6914fc808dfd1f87b42558
pilgyu2003 <pilgyu2003@gmail.com>
2024-01-10 14:30:28 +0900

    --- Auto Git Commit ---

--- C_winter/10.01.24/main.c
+++ C_winter/10.01.24/main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 
-void print_stars() {
-    for (int i = 0; i < 30; i++) {
-        printf("*");
+// void print_stars() {
+//     for (int i = 0; i < 30; i++) {
+//         printf("*");
+//     }
+// }
+
+// int main(void) {
+
+//     print_stars();
+//     printf("\n");
+//     printf("Hello, World!\n");
+//     print_stars();
+//     printf("\n");
+
+//     return 0;
+
+// }
+
+int max(int x, int y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
     }
 }
 
 int main(void) {
-
-    print_stars();
-    printf("\n");
-    printf("Hello, World!\n");
-    print_stars();
-    printf("\n");
-
+    int a = 10;
+    int b = 20;
+    int c = max(a, b);
+    printf("max = %d\n", c);
     return 0;
-
 }
