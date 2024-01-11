c16124ff98f5d97f7c7357d659e3c115efa3488c
pilgyu2003 <pilgyu2003@gmail.com>
2024-01-11 15:54:20 +0900

    --- Auto Git Commit ---

--- C_winter/11.01.24/main.c
+++ C_winter/11.01.24/main.c
@@ -4,11 +4,11 @@ extern unsigned random_i(void);
 extern double random_f(void);
 extern int MOD; 
 
-int main(void)
-{
-int i;
-MOD = 32767; // 외부 연결된 M값 32767로 변경
-for (i = 0; i < 10; i++)
-printf("%d ", random_i());
-return 0;
+int main(void) {
+    int i;
+    MOD = 32767;
+    for (i = 0; i < 10; i++) {
+        printf("%d ", random_i());
+    }
+    return 0;
 }
