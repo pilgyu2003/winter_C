9c26760312ea38e92cdc11713d062998f8335a9b
pilgyu2003 <pilgyu2003@gmail.com>
2024-01-11 14:17:36 +0900

    --- Auto Git Commit ---

--- C_winter/11.01.24/main.c
+++ C_winter/11.01.24/main.c
@@ -5,12 +5,12 @@
 int main(void) {
 
     time_t start_time = time(NULL);
-    time_t current_time;
+    time_t end_time;
 
     // Wait for 10 seconds
     do {
-        current_time = time(NULL);
-    } while (current_time - start_time < 10);
+        end_time = time(NULL);
+    } while (end_time - start_time < 10);
 
     printf("10초가 지났습니다.\n");
 
