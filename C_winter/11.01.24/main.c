ad2aa2d3a8c7b494df3ba3c4d807243ce89d6226
pilgyu2003 <pilgyu2003@gmail.com>
2024-01-11 15:53:40 +0900

    --- Auto Git Commit ---

--- C_winter/11.01.24/main.c
+++ C_winter/11.01.24/main.c
@@ -1,8 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-
-int main(void) {
-
-   
+extern unsigned random_i(void);
+extern double random_f(void);
+extern int MOD; // extern 사용하여 변수 M 외부 연결됨
+int main(void)
+{
+int i;
+MOD = 32767; // 외부 연결된 M값 32767로 변경
+for (i = 0; i < 10; i++)
+printf("%d ", random_i());
+return 0;
 }
