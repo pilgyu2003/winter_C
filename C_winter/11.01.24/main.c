2480ba3307981dffa3ee6fb890811e14c4543755
pilgyu2003 <pilgyu2003@gmail.com>
2024-01-11 14:17:11 +0900

    --- Auto Git Commit ---

--- C_winter/11.01.24/main.c
+++ C_winter/11.01.24/main.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 int main(void) {
-    system("dir");
-    printf("아무키나 치세요\n");
-    getchar();
-    system("clear");
+
+    time_t start_time = time(NULL);
+    time_t current_time;
+
+    // Wait for 10 seconds
+    do {
+        current_time = time(NULL);
+    } while (current_time - start_time < 10);
+
+    printf("10초가 지났습니다.\n");
 
     return 0;
 }
