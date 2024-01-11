db80dd406d64b61f9b5c90089630dae23f2ecc77
pilgyu2003 <pilgyu2003@gmail.com>
2024-01-11 14:26:50 +0900

    --- Auto Git Commit ---

--- C_winter/11.01.24/main.c
+++ C_winter/11.01.24/main.c
@@ -4,17 +4,5 @@
 
 int main(void) {
 
-    time_t start_time = time(NULL);
-    time_t end_time;
-
-    printf("엔터를 눌러 시작하세요.\n");
-    getchar(); // Wait for user to press Enter
-
-    do {
-        end_time = time(NULL);
-    } while (end_time - start_time < 10);
-
-    printf("10초가 지났습니다.\n");
-
-    return 0;
+   
 }
